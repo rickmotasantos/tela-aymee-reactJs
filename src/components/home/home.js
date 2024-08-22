c9b962c3dfdfb80a3605852a5b8c355d41e6bea5
Ricardo Mota <ricardo34mota@gmail.com>
2024-08-22 09:13:55 -0300

    adicionadotodos os arquivos e pastas

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import home from './home.css'
+
+const Home = () => {
+    return(
+        <> 
+            <nav>
+                <a href=''>Home</a>
+                <a href=''>sobre</a>
+                <a href=''>Blog</a>
+                <a href=''>Contatos</a>
+            </nav>
+        </>
+    )
+}
+
+export default Home
